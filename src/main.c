7fab583529b25e5ee5c8d0c5317ef6e0a75f4ecc
gvarys <johnnyzaev@icloud.com>
2022-02-26 19:23:52 +0300

    errors func and argc check

--- src/main.c
+++ src/main.c
@@ -6,11 +6,22 @@
 /*   By: gvarys <gvarys@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 15:21:46 by gvarys            #+#    #+#             */
-/*   Updated: 2022/02/25 16:18:33 by gvarys           ###   ########.fr       */
+/*   Updated: 2022/02/26 19:23:05 by gvarys           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int errors(int err)
+{
+	if (err == 1)
+		printf("Malloc error\n");
+	if (err == 2)
+		printf("Mlx error\n");
+	if (err == 3)
+		printf("Arguments error\n");
+	exit(err);
+}
 
 int	main(int argc, char **argv)
 {
@@ -18,10 +29,13 @@ int	main(int argc, char **argv)
 	void	*win;
 	int		size_x;
 	int		size_y;
-	(void)argc;
 	(void)argv;
 
+	if (argc != 2)
+		errors(3);
 	mlx = mlx_init();
+	if (!mlx)
+		errors(2);
 	mlx_get_screen_size(&size_x, &size_y);
 	win = mlx_new_window(mlx, size_x, size_y, "cub3D");
 	mlx_loop(mlx);
