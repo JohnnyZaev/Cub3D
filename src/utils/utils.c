556460fb044dcd12fb7541286d5efc52c39dffd3
gvarys <johnnyzaev@icloud.com>
2022-05-04 18:37:55 +0300

    utils norm fix

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,16 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ereginia <ereginia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/25 15:21:46 by gvarys            #+#    #+#             */
+/*   Updated: 2022/04/29 15:38:13 by ereginia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 double	ft_norm(double ray_ang)
 {
 	ray_ang = remainder(ray_ang, 2 * M_PI);
-	ray_ang += ray_ang < 0 ? 2 * M_PI : 0;
+	if (ray_ang < 0)
+		ray_ang += 2 * M_PI;
 	return (ray_ang);
 }
 
-int		is_wall(double y, double x, t_mlx_god *god)
+int	is_wall(double y, double x, t_mlx_god *god)
 {
-	if (x >= 0 && x < (float)god->map_size_x * TAIL_SIZE && y >= 0 &&
-		y < (float)god->map_size_y * TAIL_SIZE)
+	if (x >= 0 && x < (float)god->map_size_x * TAIL_SIZE && y >= 0
+		&& y < (float)god->map_size_y * TAIL_SIZE)
 	{
 		if (god->map[(int)y / TAIL_SIZE][(int)x / TAIL_SIZE] != '1')
 			return (1);
@@ -18,7 +31,7 @@ int		is_wall(double y, double x, t_mlx_god *god)
 	return (0);
 }
 
-double		distance_bew_points(double x, double y, double xd, double yd)
+double	distance_bew_points(double x, double y, double xd, double yd)
 {
 	return (sqrt(pow((xd - x), 2) + pow(yd - y, 2)));
 }
