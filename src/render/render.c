21bf293c686b7ae7e7a74e18c1d537761627d190
gvarys <johnnyzaev@icloud.com>
2022-05-05 19:46:08 +0300

    norm fix for render.c

--- src/render/render.c
+++ src/render/render.c
@@ -22,25 +22,32 @@ static void	renha(t_mlx_god *god)
 	{
 		if (god->rays[god->render->i].hitvert
 			&& god->rays[god->render->i].right)
-			god->img.data[(int)god->render->i + (int)god->render->j * god->size_x]
-			= (god->texturep[0][TAIL_SIZE * god->txty + god->txtx]);
+			god->img.data[(int)god->render->i
+				+ (int)god->render->j * god->size_x]
+				= (god->texturep[0][TAIL_SIZE * god->txty + god->txtx]);
 		if (god->rays[god->render->i].hitvert && god->rays[god->render->i].left)
-			god->img.data[(int)god->render->i + (int)god->render->j * god->size_x] =
-					god->texturep[1][TAIL_SIZE * god->txty + god->txtx];
+			god->img.data[(int)god->render->i
+				+ (int)god->render->j * god->size_x]
+				= god->texturep[1][TAIL_SIZE * god->txty + god->txtx];
 		if (!god->rays[god->render->i].hitvert && god->rays[god->render->i].up)
-			god->img.data[(int)god->render->i + (int)god->render->j * god->size_x] =
-					god->texturep[2][TAIL_SIZE * god->txty + god->txtx];
-		if (!god->rays[god->render->i].hitvert && god->rays[god->render->i].down)
-			god->img.data[(int)god->render->i + (int)god->render->j * god->size_x] =
-					god->texturep[3][TAIL_SIZE * god->txty + god->txtx];
+			god->img.data[(int)god->render->i
+				+ (int)god->render->j * god->size_x]
+				= god->texturep[2][TAIL_SIZE * god->txty + god->txtx];
+		if (!god->rays[god->render->i].hitvert
+			&& god->rays[god->render->i].down)
+			god->img.data[(int)god->render->i
+				+ (int)god->render->j * god->size_x]
+				= god->texturep[3][TAIL_SIZE * god->txty + god->txtx];
 	}
 }
 
-static void		renhel(t_mlx_god *god)
+static void	renhel(t_mlx_god *god)
 {
-	god->render->raydist = god->rays[god->render->i].distance *
-						   cos(god->rays[god->render->i].angle - (god->player->angle * (M_PI/180)));
-	god->render->wallstrip = (TAIL_SIZE / god->render->raydist) * god->render->distpj;
+	god->render->raydist = god->rays[god->render->i].distance
+		* cos(god->rays[god->render->i].angle
+			- (god->player->angle * (M_PI / 180)));
+	god->render->wallstrip = (TAIL_SIZE / god->render->raydist)
+		* god->render->distpj;
 	god->render->top = ((float)god->size_y / 2) - (god->render->wallstrip / 2);
 	god->render->bottom = god->render->top + god->render->wallstrip;
 	god->render->j = 0;
@@ -51,19 +58,20 @@ static void		renhel(t_mlx_god *god)
 	while (god->render->j < god->render->top)
 	{
 		god->render->distancetop = god->render->j - god->render->top;
-		god->txty = (int)((float)TAIL_SIZE / god->render->wallstrip * god->render->distancetop)
-					& (TAIL_SIZE - 1);
-		if (god->render->i >= 0 && god->render->i < god->size_x && god->render->j >= 0 && god->render->j < god->size_y)
-			god->img.data[(int)god->render->i + (int)god->render->j * god->size_x] =
-					god->c_color;
+		god->txty = (int)((float)TAIL_SIZE / god->render->wallstrip
+				* god->render->distancetop) & (TAIL_SIZE - 1);
+		if (god->render->i >= 0 && god->render->i < god->size_x
+			&& god->render->j >= 0 && god->render->j < god->size_y)
+			god->img.data[(int)god->render->i + (int)god->render->j
+				* god->size_x] = god->c_color;
 		god->render->j++;
 	}
 }
 
-void		render3d(t_mlx_god *god)
+void	render3d(t_mlx_god *god)
 {
 	god->render->i = 0;
-	god->render->distpj = ((float)god->size_x / 2) / (tan((30 * (M_PI/180))));
+	god->render->distpj = ((float)god->size_x / 2) / (tan((30 * (M_PI / 180))));
 	while (god->render->i < god->size_x)
 	{
 		renhel(god);
@@ -75,12 +83,13 @@ void		render3d(t_mlx_god *god)
 		while (god->render->j < god->size_y)
 		{
 			god->render->distancetop = god->render->j - god->render->top;
-			god->txty = (int)((float)TAIL_SIZE / god->render->wallstrip * god->render->distancetop)
-						& (TAIL_SIZE - 1);
-			if (god->render->i >= 0 && god->render->i < god->size_x && god->render->j >= 0 &&
-				god->render->j < god->size_y)
-				god->img.data[(int)god->render->i + (int)god->render->j * god->size_x] =
-						god->f_color;
+			god->txty = (int)((float)TAIL_SIZE
+					/ god->render->wallstrip * god->render->distancetop)
+				& (TAIL_SIZE - 1);
+			if (god->render->i >= 0 && god->render->i < god->size_x
+				&& god->render->j >= 0 && god->render->j < god->size_y)
+				god->img.data[(int)god->render->i + (int)god->render->j
+					* god->size_x] = god->f_color;
 			god->render->j++;
 		}
 		god->render->i++;
